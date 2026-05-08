bfaa07b01a36f76184db20a227d07b0118397132
Ian <107736504+IanPess@users.noreply.github.com>
2026-05-07 21:40:52 -0300

    Add observation field to task addition functionality

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
- let tarefas = [];
+let tarefas = [];
 function Adicionar(){
     var tituloid = document.getElementById("titulo");
     var descricaoid = document.getElementById("descricao");
     var prioridadeid = document.getElementById("prioridade");
     var dataLimiteid = document.getElementById("dataLimite");
     var statusid = document.getElementById("status");
+    var observacao = document.getElementById("Obs");
 
-
-    if (tituloid.value != "" && prioridadeid.value != "" && dataLimiteid.value != "" && statusid.value != "") {
-        alert('Os dados foram encaminhados com sucesso');
-    }else{
-         alert('Cadastre todos os dados Obrigatorios');
-    }
-    novaTarefa = {
+    let novaTarefa = {
         titulo : tituloid.value,
         descricao : descricaoid.value,
         prioridade : prioridadeid.value,
         dataLimite : dataLimiteid.value,
-        status : statusid.value
+        status : statusid.value,
+        observacao: observacao ? observacao.value : ""
 }
-tarefas.push(novaTarefa)
-const tbody = document.querySelector("#areaTabela");
-    
-    // Limpa a tabela
-    tbody.innerHTML = "";
- 
-    // Adiciona linhas dinamicamente
-    tarefas.forEach(item => {
-        let tr = document.createElement("tr");
-        tr.innerHTML = `<td>${item.titulo}</td><td>${item.descricao}</td><td>${item.prioridade}</td><td>${item.dataLimite}</td><td>${item.status}</td>`;
-        tbody.appendChild(tr);
-    });
+if (tituloid.value != "" && prioridadeid.value != "" && dataLimiteid.value != "" && statusid.value != "") {
+        alert('Os dados foram encaminhados com sucesso');
+        tarefas.push(novaTarefa)
+    }else{
+         alert('Cadastre todos os dados Obrigatorios');
+    }
+
+}
+function Remover(indice){
+    tarefas.splice(indice,1)
 }
 
 
