a3beb136efd108b7de6c2779a8dac74175c9a9fe
Ian <107736504+IanPess@users.noreply.github.com>
2026-05-14 20:38:25 -0300

    Enhance task management with edit and local storage
    Added functionality to edit tasks and save to local storage.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let tarefas = [];
+let editando = null;
 function Adicionar(){
     var tituloid = document.getElementById("titulo");
     var descricaoid = document.getElementById("descricao");
@@ -16,16 +17,59 @@ function Adicionar(){
         observacao: observacao ? observacao.value : ""
 }
 if (tituloid.value != "" && prioridadeid.value != "" && dataLimiteid.value != "" && statusid.value != "") {
-        alert('Os dados foram encaminhados com sucesso');
-        tarefas.push(novaTarefa)
+        if(editando == null){
+        tarefas.push(novaTarefa);
+        SalvarLocalStorage();
+        LimparFormulario();
+    }else{
+        tarefas[editando] = novaTarefa;
+        editando = null;
+        SalvarLocalStorage();
+        LimparFormulario();
+    }
     }else{
          alert('Cadastre todos os dados Obrigatorios');
     }
+    
+    
 
 }
 function Remover(indice){
     tarefas.splice(indice,1)
+    SalvarLocalStorage();
 }
+function LimparFormulario(){
+   document.getElementById("titulo").value = "";
+    document.getElementById("descricao").value = "";
+    document.getElementById("prioridade").value = "";
+    document.getElementById("dataLimite").value = "";
+    document.getElementById("status").value = "";
+   let obs = document.getElementById("Obs");
+   if(obs != null){
+    obs.value = "";
+   }
+    editando = null
+}
+
+function SalvarLocalStorage(){
+    localStorage.setItem(
+    "tarefas",
+    JSON.stringify(tarefas)
+)}
+function CarregarLocalStorage() {
+
+    let dados = localStorage.getItem("tarefas");
+
+    if (dados) {
+        tarefas = JSON.parse(dados);
+    } else {
+        tarefas = [];
+    }
+
+    RenderizarTabela(tarefas);
+}
+window.addEventListener("DOMContentLoaded", CarregarLocalStorage);
+
 
 
     
